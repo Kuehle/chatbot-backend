6e495857eded4186e0d9d6a98fb51b97db7a70fe
Johannes Kühlewindt <johannes.kuehlewindt@bytabo.de>
2017-11-23 14:02:29 +0100

    socket io

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var io = require('socket.io');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.io = io();
+
+app.io.on('connection', (socket) => {
+  console.log('user connected')
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
