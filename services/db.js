0bd74112f6da179f73313f0cce8271a2bf55b65f
Johannes Kühlewindt <johannes.kuehlewindt@bytabo.de>
2017-11-26 17:03:20 +0100

    db service get random

--- services/db.js
+++ services/db.js
@@ -1,23 +1,39 @@
-// {
-//     "size": 1,
-//     "query": {
-//        "function_score": {
-//           "functions": [
-//              {
-//                 "random_score": {
-//                    "seed": "1477072619038"
-//                 }
-//              }
-//           ]
-//        }
-//     }
-//  }
+const elasticsearch = require('elasticsearch')
+const client = new elasticsearch.Client({
+    host: 'localhost:9200'
+})
 
-// hits.hits[0]
+function search(searchObj) {
+    return new Promise((resolve, reject) => {
+        client.search(searchObj).then((body) => {
+            resolve(body.hits.hits)
+        }, (err) => {
+            console.trace(error.message)
+            reject(err)
+        })
+    })
+}
 
 let dbService = {
     random: () => {
-
+        return search({
+            index: 'bot',
+            type: 'meals',
+            body: {
+                "size": 1,
+                "query": {
+                   "function_score": {
+                      "functions": [
+                         {
+                            "random_score": {
+                               "seed": '' + Math.random()
+                            }
+                         }
+                      ]
+                   }
+                }
+             }
+        })
     }
 }
 
--- services/db.spec.js
+++ services/db.spec.js
@@ -9,8 +9,16 @@ describe('DataBase Service', () => {
     it('should export a function', () => {
       expect(db.random).to.be.a('function')
     }),
-    it('should return a document', () => {
-      expect(db.random()).to.have.property('mealId')
+    it('should return a promise', () => {
+      expect(db.random()).to.be.a('Promise')
+    }),
+    it('should return exactly one document', async () => {
+      let dbResponse = await db.random()
+      expect(dbResponse.length).to.equal(1)
+    }),
+    it('should be a meal', async () => {
+      let dbResponse = await db.random()
+      expect(dbResponse[0]._source).to.have.property('mealId')
     })
   })
 })
